f8e4f1f386b9388e8ec700ef86ffaa33883a05f7
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-09-14 21:05:55 +0530

    Created tables and a superuser login.
    Created all tables in mysite\Specialtopic\models.py, add all the tables to be viewed in by admin or superuser in mysite\Specialtopic\admin.py.
    
    Also added a superuser, to access it run the manage.py file in shell using this command 'python manage.py runserver' and then add the '/admin' to the end of the link given in the shell.

--- mysite/Specialtopic/admin.py
+++ mysite/Specialtopic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# this import from models.py file which stores all the sql tables.
+from .models import Student_database, System_database
+
+# This adds both the tables from models.py into admin site for viewing or adding.
+admin.site.register(Student_database)
+admin.site.register(System_database)
