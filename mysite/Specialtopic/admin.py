81dc361616cb8205cc78ffa217e4d5334c6a8869
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-09-16 12:11:59 +0530

    Added a display and filter.

--- mysite/Specialtopic/admin.py
+++ mysite/Specialtopic/admin.py
@@ -4,9 +4,23 @@ from  django.contrib.auth.models  import  Group
 # this import from models.py file which stores all the sql tables.
 from .models import Student_database, System_database
 
+
+# This class is to display all the values in the Student_database table.
+class  Student_databaseAdmin(admin.ModelAdmin):
+    list_display=("USN", "Student_name")
+    
+
+# This class is to display all the values in the System_database table.
+class  System_databaseAdmin(admin.ModelAdmin):
+    list_display=("Date", "Student_name", "Branch", "USN", "System_no", "Time_in", "Time_out")
+    # creates a filter to sort by. 
+    list_filter = ("Date",)
+
+
 # This adds both the tables from models.py into admin site for viewing or adding.
-admin.site.register(Student_database)
-admin.site.register(System_database)
+admin.site.register(Student_database, Student_databaseAdmin)
+admin.site.register(System_database, System_databaseAdmin)
+
 
 # Changes the name of the header, title and index.
 admin.site.site_title  =  "System Assign Admin"
@@ -16,5 +30,3 @@ admin.site.index_title  =  "Admin Controls"
 # Removes all default groups.
 admin.site.unregister(Group)
 
-class  detailsAdmin(admin.ModelAdmin):
-    list_display=('book_name','category','Author','pages','publisher')
