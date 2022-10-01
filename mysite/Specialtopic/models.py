181c8d0e9b4c16979e28c220e4ba14c5b182c8f1
Adithya GS <70025415+zephyr-0001@users.noreply.github.com>
2022-10-01 19:06:50 +0530

    Added "mysite" folder back to admin end
    Some bug happened and admin end wasn't working at all. I don't know man. I'm just reuploading it because I hadn't pulled after it happened.
    You're welcome :D

---
+++ mysite/Specialtopic/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from datetime import datetime
+
+
+# Name of the class will be the name of the table in sqlite database.
+# Each variable created in a class is a column name with the specific datatype.
+
+# This table holds all the student names with there USN.
+class Student_database(models.Model):
+    Student_name = models.CharField(max_length = 50)
+    USN = models.CharField(max_length = 11)
+
+# This table holds all the student name and the system they used and the in time and out time.
+class System_database(models.Model):
+    Date = models.DateField(auto_now_add=True, blank=True)
+    Student_name = models.CharField(max_length = 50)
+    Branch = models.CharField(max_length = 10)
+    USN = models.CharField(max_length = 11)
+    System_no = models.CharField(max_length = 9)
+    Time_in = models.TimeField()
+    Time_out = models.TimeField()
