f45f3e08bc680f06731226fc13770dde401236fb
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-10-03 21:26:00 +0530

    Added foreign and primary key.

--- mysite/Specialtopic/models.py
+++ mysite/Specialtopic/models.py
@@ -8,23 +8,25 @@ from datetime import datetime
 # This table holds all the student names with there USN.
 class Student_database(models.Model):
     Student_name = models.CharField(max_length = 50)
-    USN = models.CharField(max_length = 11)
+    USN = models.CharField(max_length = 11, primary_key=True)
 
 # This table holds all the student name and the system they used and the in time and out time.
 class System_database(models.Model):
     Date = models.DateField(auto_now_add=True, blank=True)
     Student_name = models.CharField(max_length = 50)
 
+    # This is list of pre-defined courses.
     courses=(
         ('cs','CSE'),
         ('ec','ECE'),
         ('as','ASE'),
         ('me','Mech')
-        
     )
-    Branch = models.CharField(choices=courses,max_length = 10)
-    USN = models.CharField(max_length = 11)
 
+    Branch = models.CharField(choices=courses,max_length = 10)
+    USN = models.ForeignKey(Student_database, on_delete=models.CASCADE)
+
+    # This is list of pre-defined systems.
     systems=(
         ('s1','System1'),
         ('s2','System2'),
@@ -33,6 +35,7 @@ class System_database(models.Model):
         ('k1','Kindle1'),
         ('k2','Kindle2')
     )
+
     System_no = models.CharField(choices=systems,max_length = 9)
     Time_in = models.TimeField()
     Time_out = models.TimeField()
