bc0c7ac0965b9128608f67fc0aec08d0ff88e3c9
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-11-06 20:15:22 +0530

    ApScheduler library
    For this to work you need to install ApScheduler -  $ pip install apscheduler
    
    I have created and updater_excel which is scheduled at a particular time to run and also configured the app.py present in Specialtopic root directory.
    
    This is import so the scheduler can run when the site is on.

--- mysite/Specialtopic/apps.py
+++ mysite/Specialtopic/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SpecialtopicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Specialtopic'
+
+    def ready(self):
+        from Scheduler import updater_excel
+        updater_excel.start()
