472d41e5dec5c22e8328a4ae0cbaedf1edaf2e39
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-11-06 20:12:00 +0530

    Created an API
    This file excel_creator just call the existing System_database using import_export module which extracts from the admin.
    
    The function inside "trial", just extracts the data and converts it to excel.

--- mysite/Scheduler/excel_creator.py
+++ mysite/Scheduler/excel_creator.py
@@ -1,3 +1,25 @@
-# import sys
-# sys.path.insert(1, 'C:/Users/Devis/Documents/Special-topics/mysite/Specialtopic')
-print("Hello")
+import sys
+import xlsxwriter
+sys.path.append('../')
+
+from Specialtopic.admin import Student_databaseResource, System_databaseResource
+
+def trial():
+    dataset = Student_databaseResource().export()
+    dataset = str(dataset.csv).split('\r\n')
+    del dataset[-1]
+    
+    
+    book = xlsxwriter.Workbook('Example2.xlsx')
+    sheet = book.add_worksheet()
+    row = 0
+
+
+
+    for value in dataset:
+        col_1, col_2 = value.split(",")
+        sheet.write(row, 0, col_1)
+        sheet.write(row, 1, col_2)
+        row += 1
+    book.close()
+    
